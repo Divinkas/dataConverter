8bbdd24193954e22d1af91d52510b16731e734f5
divin <divinkas@gmail.com>
2019-03-05 15:46:58 +0200

    add model to base mapper

--- app/src/main/java/com/divinkas/example/fasadeproject/utils/mapper/BaseMapper.java
+++ app/src/main/java/com/divinkas/example/fasadeproject/utils/mapper/BaseMapper.java
@@ -7,6 +7,12 @@ import com.divinkas.example.fasadeproject.utils.ICallbackConverter;
 import com.divinkas.example.fasadeproject.viewmodel.response.BaseResponse;
 
 public abstract class BaseMapper<T> {
+    protected T model;
+
+    public BaseMapper(T model) {
+        this.model = model;
+    }
+
     public abstract void onMapped(BaseResponse response, ICallbackConverter<T> callback);
 
     protected void sendToMain(T model, ICallbackConverter<T> callback){
--- app/src/main/java/com/divinkas/example/fasadeproject/utils/mapper/TicketMapper.java
+++ app/src/main/java/com/divinkas/example/fasadeproject/utils/mapper/TicketMapper.java
@@ -6,13 +6,14 @@ import com.divinkas.example.fasadeproject.viewmodel.response.BaseResponse;
 import com.divinkas.example.fasadeproject.viewmodel.response.TicketResponse;
 
 public class TicketMapper extends BaseMapper<TicketModel> {
+
     public TicketMapper() {
+        super(new TicketModel());
     }
 
     @Override
     public void onMapped(BaseResponse response, ICallbackConverter<TicketModel> callback) {
         TicketResponse response_ticket = (TicketResponse) response;
-        TicketModel model = new TicketModel();
 
         model.setId(response_ticket.getIdTicket());
         model.setName(response_ticket.getNameTicket());
